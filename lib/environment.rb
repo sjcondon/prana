089dfbaf5c2c44696ecd5418d613eae146025c0f
Sarah Condon <s.j.condon01@gmail.com>
2019-10-27 18:44:16 -0500

    first commit

---
+++ lib/environment.rb
@@ -0,0 +1,12 @@
+require "prana/version"
+require_relative './prana/cli'
+require_relative './prana/discounts'
+require_relative './prana/scraper'
+require "pry"
+require "nokogiri"
+require "open-uri"
+
+module Prana
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/prana/version.rb
@@ -0,0 +1,3 @@
+module Prana
+  VERSION = "0.1.0"
+end
