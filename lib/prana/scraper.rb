f0428701209cfca7557d124473964f159534e884
Sarah Condon <s.j.condon01@gmail.com>
2019-10-27 19:26:49 -0500

    add nokogiri

--- lib/prana/scraper.rb
+++ lib/prana/scraper.rb
@@ -1,2 +1,6 @@
 class Prana::Scraper
+   def self.scrapes_discounts 
+    html = "https://www.prana.com/sale/womens-sale.html#prefv2=XS%7C00&prefv1=5%20Stars%7C4%20Stars%20%26%20Up&prefn2=size&prefn1=reviewRating"
+    doc = Nokogiri::HTML(html)
+   end
 end
