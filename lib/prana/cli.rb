c2c2e5ced55c4f9c238f72be3983cf2b7e10475a
Sarah Condon <s.j.condon01@gmail.com>
2019-10-29 19:12:12 -0500

    add comments

--- lib/prana/cli.rb
+++ lib/prana/cli.rb
@@ -2,33 +2,29 @@ class Prana::CLI
     
     def call
         Prana::Scraper.scrapes_discounts #want to scrape once
+        @clothing = Prana::Clothing.all #we want all of the clothing objects to go into the clothing
         puts "Enjoy these discounts!"
         start #to start the program
         
         
     end
     #names methods
-    def start 
-        get_clothing
-        list_price
+    def start         
+        list_clothing
         product_discription
-    end
-
-    def get_clothing 
-        @clothing = Prana::Clothing.all
-        #@clothing is a class variable
-    end
+        do_next
+    end    
     
-    def list_price
+    def list_clothing
         puts "Choose the clothing you would like to view."
-        @clothing.each.with_index(1) do |clothing,index|
-            puts "#{index}. #{clothing.name}"
+        @clothing.each.with_index(1) do |clothing,index| #iterates via clothing array
+            puts "#{index}. #{clothing.name}" #lists all of the clothings
         end
     end
     
     def product_discription
         chosen_number = gets.strip.to_i
-        show_clothing_for(chosen_number) if valid_input(chosen_number, @clothing)
+        show_clothing_for(chosen_number) if valid_input(chosen_number, @clothing) #taking from the show_clothing method
     end
 
     def valid_input(input,data)
@@ -39,16 +35,20 @@ class Prana::CLI
         clothing = @clothing[chosen_number -1]
         puts "Here is your clothing for the #{clothing.name}..."
         puts "#{clothing.price}"
-end
+    end
+
     def do_next
         puts "Would you want to look at another item? y/n"
-        input = gets.strip
+
+        input = gets.strip #get users input and strip takes out white space before and after what the user types. 
+#then saving users input into a variable called "input"
+
         if input == "y"
             start #you will call start
         elsif input == "n"
-        exit #leave the program
+            exit #leave the program
         else 
-            puts "Enter something please"
+            puts "I don't understand"
             do_next 
         end
     end
