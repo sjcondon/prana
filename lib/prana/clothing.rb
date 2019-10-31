a7c3bff04552652d2d080fc7409864097e5c1051
Sarah Condon <s.j.condon01@gmail.com>
2019-10-31 14:05:43 -0500

    making sure clothing get's pushed

---
+++ lib/prana/clothing.rb
@@ -0,0 +1,23 @@
+class Prana::Clothing
+    attr_accessor :name, :url, :price, :description
+
+    @@all = []
+    
+    def initialize(name,price,url)
+        @name = name
+        @price = price
+        @url = url 
+        @description = []
+        @@all << self 
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    ## testing , is this working?
+
+end
+
+### Clothing.new(name,url,price)
